5ab70fcbe33ad103fb80ac9696de6bca28fde797
adanyc <adanyc@gmail.com>
2025-04-23 15:21:41 -0500

    initial commit

---
+++ src/app/views/atmosphere/atmosphere.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atmosphere',
+  standalone: true,
+  templateUrl: './atmosphere.component.html',
+})
+export class AtmosphereComponent { }
